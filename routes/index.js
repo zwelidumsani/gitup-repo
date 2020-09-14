8f4a1d23f0f05b1f9a81325a8243af2f8287dc2f
zwelidumsani <zwelidumsani@gmail.com>
2020-09-14 09:38:53 +0200

    changed

--- routes/index.js
+++ routes/index.js
@@ -259,6 +259,13 @@ router.get('/wealth', function(req, res, next){
 });
 
 
+router.get('/all-products', function(req, res, next){
+	 req.session.listingUrl = '/all-products';
+	 Product.find(function(err, docs){
+		 return res.render('listing', {title: 'Shopping Cart',headin: "AVAILABLE STOCK",products: docs});
+	 });
+});
+
 router.get('/product-d/:id', (req, res) => {
     proId = req.params.id;
      res.redirect('/product-details');	
@@ -420,6 +427,7 @@ router.post('/signin',passport.authenticate('local', {
 	
 
 router.get('/about', function(req, res){
+	
 		res.render('aboutUs.handlebars');
 });
 
